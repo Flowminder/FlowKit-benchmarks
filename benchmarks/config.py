65043a1026a798a10a5163b6b62b007234a59194
James Harrison <james.harrison@flowminder.org>
2019-01-28 17:10:42 +0000

    Add MPLv2 license headers

--- benchmarks/config.py
+++ benchmarks/config.py
@@ -1,13 +1,6 @@
-#
-# FLOWDB_BENCHMARK_INSTANCES describes what kind of synthetic CDR
-# data should be installed in the flowdb instances. Currently there
-# is only a single instance but we will likely want multiple ones
-# in the future.
-#
-# Note: the port(s) specified below need to be kept in sync with the
-#       ports provided as environment variables to the docker-compose
-#       file used for bringing up the benchmark docker containers.
-#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 FLOWDB_CONFIG_PARAM_NAMES = ["num_days", "num_subscribers", "num_cells", "num_calls_per_day", "analyze", "cluster", "jit"]
 
