bb09870cf9a7570e968a5b39d9cb7f32a705401d
Jono Gray <jonathan.gray@flowminder.org>
2019-02-26 13:56:23 +0000

    Add optional cleanup of containers and data after a benchmark run

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -8,3 +8,6 @@
 # the --config switch. There will be no error, it will simply use the
 # user-provided value.
 pipenv run asv run $@
+
+[ -e containers_to_clean_up ] && docker stop $(cat containers_to_clean_up) && rm containers_to_clean_up
+[ -e dirs_to_clean_up ] && rm -rf $(cat dirs_to_clean_up) && rm dirs_to_clean_up
